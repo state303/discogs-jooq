754a155a3e446bde2df1279b0ced316c1d319457
state303 <shbae0121@gmail.com>
2021-06-23 23:39:54 +0900

    applies builder pattern from lombok to label entity

--- src/main/java/io/dsub/discogs/common/entity/master/Master.java
+++ src/main/java/io/dsub/discogs/common/entity/master/Master.java
@@ -5,15 +5,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.With;
+
+import lombok.*;
 
 @Data
 @Entity
-@With
+@Builder
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
